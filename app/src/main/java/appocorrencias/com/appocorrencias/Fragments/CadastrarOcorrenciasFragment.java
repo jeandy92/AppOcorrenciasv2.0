d044dde49dbb6252f5302124eb0b0d102c462000
jeandy92 <jeandymlameida@gmail.com>
2017-04-22 16:57:52 -0300

    Versão 26 -  22/04/2017

--- app/src/main/java/appocorrencias/com/appocorrencias/Fragments/CadastrarOcorrenciasFragment.java
+++ app/src/main/java/appocorrencias/com/appocorrencias/Fragments/CadastrarOcorrenciasFragment.java
@@ -30,7 +30,7 @@ public class CadastrarOcorrenciasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
 
-        View v = inflater.inflate(R.layout.fragment_cadastrar_ocorrencias, container, false);
+        View v = inflater.inflate(R.layout.fragment_ocorrencias_registradas, container, false);
         ListView lstItems = (ListView)v.findViewById(R.id.lvListaOcorrencias);
 
         ArrayList<OcorrenciasRegistradas> prueba = criarocorrencias();
@@ -49,7 +49,7 @@ public class CadastrarOcorrenciasFragment extends Fragment {
         return v;
 
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_cadastrar_ocorrencias, container, false);
+        //return inflater.inflate(R.layout.fragment_ocorrencias_registradas, container, false);
 
 
 
